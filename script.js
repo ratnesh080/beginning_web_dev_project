806fcd09e4871dceb39f9a7db822fa4f9176b67a
Ratnesh Kumar Yadav <126163660+ratnesh080@users.noreply.github.com>
2025-07-19 16:51:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// var typed= new Typed(".multiple-text",{
+//     strings: ["FrontEnd Developer ", "Backend Developer" , "Graphic Designer"],
+//     typeSpeed:100,
+//     backSpeed:100,
+//     backDelay: 1000,
+//     loop:true
+// =======
+var typed= new Typed(".multiple-text",{
+    strings: ["FrontEnd Developer ", "Backend Developer" , "AI/ML Engineer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay: 1000,
+    loop:true
+
+})
